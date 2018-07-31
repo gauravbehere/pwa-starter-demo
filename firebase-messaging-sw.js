cb7bde50e1711feff314f9c9d6d0a411b62b6864
gauravbehere <gaurav.techgeek@gmail.com>
2018-07-31 13:06:15 +0530

    Adding worker for fb

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,9 @@
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
+firebase.initializeApp({
+  'messagingSenderId': '824572363258'
+});
+const messaging = firebase.messaging();
