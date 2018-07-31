efe37e75347144767c34f58f8b681e1aec3047a3
gauravbehere <gaurav.techgeek@gmail.com>
2018-07-31 11:38:36 +0530

    correcting firebase key

--- js/push.js
+++ js/push.js
@@ -1,5 +1,5 @@
 var messaging = firebase.messaging();
-messaging.usePublicVapidKey("AIzaSyC43JJJObG0BsLwFYIbKOeknRt1Dnz4ZPM");
+messaging.usePublicVapidKey("BDnGLUP_ncj-O0so3o5IdhCss93SyT3ZNDMiYIaM8NHC489oGwSWi4Z-ih3tyiiwCpkhZpIfu1EcEUJ0wNI37Do");
 
 messaging.onMessage(function(payload) {
     console.log('Message received. ', payload);    
