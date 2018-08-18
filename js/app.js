fd9ce5f0f717fc3df178276d3aaa440c130c8c33
Gaurav <Gaurav.techgeek@gmail.com>
2018-08-18 08:49:17 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,24 +1,30 @@
 (function () {
+    /*
+    * Registering Service Worker & listening to it's update.
+    */
     if ("serviceWorker" in navigator) {
       navigator.serviceWorker.register('./serviceWorker.js', { scope: "./" })
       .then(function(registration) {
         console.info('Service worker registered');
-        checkForPageUpdate(registration);
+        checkForUpdate(registration);
       })
       .catch(function(error) {
         console.error('Service worker registration failed ', error);
       });
     }
-    function checkForPageUpdate(registration) {          
+    function checkForUpdate(registration) {          
       registration.addEventListener("updatefound", function() {        
         if (navigator.serviceWorker.controller) {
-          var installingSW = registration.installing;
-          installingSW.onstatechange = function() {
-            console.info("Service Worker State :", installingSW.state);
-            switch(installingSW.state) {
+          var installingWorker = registration.installing;
+          installingWorker.onstatechange = function() {
+            console.info("Service Worker State :", installingWorker.state);
+            switch(installingWorker.state) {
               case 'installed':
               navigator.serviceWorker.ready
               .then(function (registration) {
+                  /*
+                  * Notify user to refresh the webpage.
+                  */
                 registration.showNotification('Site Content Updated\n Please Refresh.')
               });
                 break;
